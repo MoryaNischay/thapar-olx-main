f8b7d3f17aae5b9c587346ce8a40eac8eed23af0
MoryaNischay <nischaymorya@gmail.com>
2023-11-20 22:28:43 +0530

    Death

--- src/Components/MyPosts/mypost.js
+++ src/Components/MyPosts/mypost.js
@@ -5,14 +5,27 @@ import "./myposts.css";
 import { Firebase } from "../../firebase/config";
 import BarLoading from "../Loading/BarLoading";
 import PostCards from "../PostCards/PostCards";
-
+import { useHistory } from "react-router";
 import { AllPostContext } from "../../contextStore/AllPostContext";
 
 function MyPost() {
   const { setAllPost } = useContext(AllPostContext);
   let [posts, setPosts2] = useState([]); //for showing all posts in Descending order of date
   let [loading, setLoading] = useState(false);
+  const history = useHistory();
+  const [userDetails, setUserDetails] = useState({});
 
+  useState(() => {
+    Firebase.firestore()
+      .collection("users")
+      .where("id", "==", Firebase.auth().currentUser.uid)
+      .get()
+      .then((res) => {
+        res.forEach((doc) => {
+          setUserDetails(doc.data());
+        });
+      });
+  },[history]);
   useEffect(() => {//show only those posts which are not sold
 
     setLoading(true);
@@ -62,6 +75,18 @@ function MyPost() {
 
   return (
     <div className="postParentDiv">
+      {userDetails && (
+        <div className="userDetails">
+          <div className="heading">
+            <span>Your Details</span>
+          </div>
+          <div className="details">
+            <p>Name : {userDetails.name}</p>
+            <p>Phone : {userDetails.phone}</p>
+            
+          </div>
+        </div>
+      )}
       {posts && (
         <div className="moreView">
           <div className="heading">
@@ -77,6 +102,8 @@ function MyPost() {
           </div>
         </div>
       )}
+      {/* show the user details */}
+      
     </div>
   );
 }
